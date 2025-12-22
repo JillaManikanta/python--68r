69d0d5c9073d21e7a34a12a68f8ae6b76bd57c8e
JillaManikanta <jillamani9014@gmail.com>
2025-12-22 12:47:53 +0530

    bitwise

---
+++ BItwise.py
@@ -0,0 +1,47 @@
+#convert the integer 25 into the float value and find the result
+a=25
+b=float(a)
+print(b)
+
+#convert the float value into the integer value and print the result
+a=19.6
+b=int(a)
+print(b)
+
+#convert string "42" to integer and add 8 and print result
+a='42'
+b=int(a)
+print(b+8)
+
+#convert the string "3.14" into the float and multiply by 2
+a='3.14'
+b=float(a)
+print(b*2)
+
+#Convert the integer 100 into the string and join it with "points"
+a=100
+b=str(a)+" Points"
+print(b)
+
+
+#convert the bool vaule false into the integer and print result
+a=10
+c=(a==8)
+d=int(c)
+print(d)
+
+
+#Add integer and float and print type
+a=7+2.5
+print(type(a))
+
+
+#divide two integers and print the result
+a=7/2
+print(type(a))
+
+#covert the list into tuple
+a=[1,2,3]
+b=tuple(a)
+print(b)
+ 
